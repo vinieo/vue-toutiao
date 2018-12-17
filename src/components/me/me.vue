305db13a8b73cbf0217f52dbcee6d797deb0a315
阿猫 <41368754+vinieo@users.noreply.github.com>
2018-12-17 22:26:09 +0800

    Update me.vue

--- src/components/me/me.vue
+++ src/components/me/me.vue
@@ -21,9 +21,11 @@ export default {
   data () {
     return {
       images: [
-        
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545048186727&di=95253b5b08f21bf485d99e80fd599dbf&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201506%2F25%2F20150625202940_d8S24.jpeg',
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2768923080,2257665151&fm=26&gp=0.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545049114182&di=9fa54b5b62308f07be48bd2c40c1c420&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Fface%2Fc6578ca9ea032bc2fc0785c9e71d8f9de81bd0fd.jpg'
       ],
-      url
+      url: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2768923080,2257665151&fm=26&gp=0.jpg'
     }
   }
 }
