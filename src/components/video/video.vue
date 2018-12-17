6566b5adaa74fd218b5776d6896f773cbb6a34c3
阿猫 <41368754+vinieo@users.noreply.github.com>
2018-12-17 22:28:38 +0800

    Update video.vue

--- src/components/video/video.vue
+++ src/components/video/video.vue
@@ -1,5 +1,48 @@
 <template>
-    <div>
-        <h1>this is video!</h1>
-    </div> 
+  <div>
+    <video-player
+      class="video-player vjs-custom-skin"
+      ref="videoPlayer"
+      :playsinline="true"
+      :options="playerOptions"
+    ></video-player>
+  </div>
 </template>
+
+<script>
+import VideoPlayer from "vue-video-player";
+
+export default {
+  data() {
+    return {
+      playerOptions: {
+        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+        autoplay: false, //如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 导致视频一结束就重新开始。
+        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: "zh-CN",
+        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [
+          {
+            src: "http://gslb.miaopai.com/stream/BLaOzI2lSUdM7lFd4MIZ2g__.mp4?vend=miaopai&ssig=9a0937784bef45c7a761ecfd47f307a2&time_stamp=1545053203416&mpflag=32" //url地址
+          }
+        ],
+        poster: "https://wx2.sinaimg.cn/mw1024/81de9414gy1fw8rez7vu7j22o03k04qt.jpg", //你的封面地址
+        notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true //全屏按钮
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
