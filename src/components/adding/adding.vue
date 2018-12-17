bbaf180834f54636a5380804ead84ca7a518f09e
阿猫 <41368754+vinieo@users.noreply.github.com>
2018-12-17 23:18:28 +0800

    Update adding.vue

--- src/components/adding/adding.vue
+++ src/components/adding/adding.vue
@@ -29,7 +29,14 @@ export default {
   data () {
     return {
       list: [
-        
+        'https://wx4.sinaimg.cn/mw1024/81de9414gy1fvyz76yax2j21kw1kw4qp.jpg',
+        'https://wx1.sinaimg.cn/mw1024/81de9414gy1fy8tci5wmqj23k02o01kz.jpg',
+        'https://wx3.sinaimg.cn/mw1024/81de9414gy1fvy76r4tyyj20zk0qogoe.jpg',
+        'https://wx2.sinaimg.cn/mw1024/81de9414gy1fvy71wdd83j20qo0zkn05.jpg',
+        'https://ww3.sinaimg.cn/mw1024/81de9414gw1eyu8z61e25j2140140452.jpg',
+        'https://ww3.sinaimg.cn/mw1024/81de9414jw1exfvc77qvoj218g0p0tga.jpg',
+        'https://ww2.sinaimg.cn/mw1024/81de9414jw1evo1uep0kbj2140140na6.jpg',
+        'https://ww3.sinaimg.cn/mw1024/81de9414jw1ek2qog0zdkj20mr0h2wgy.jpg'
       ]
     }
   }
